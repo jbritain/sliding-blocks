5c353d305377972bebafcda7d5c01b8bb851c1e5
Joshua Britain <50422789+jbritain@users.noreply.github.com>
2024-04-19 13:44:13 +0100

    Revert "Merge branch 'main' into debug"
    This reverts commit fcd5413e00d20bcf43c525235a3a6d35825221ae, reversing
    changes made to 8a9be57f6e8ab6119cded89daea52069711c7131.

--- sliding.py
+++ sliding.py
@@ -1,29 +1,154 @@
 import sys
+import os
+import csv
+import cProfile
+import time
 
 from structures import *
+from solve_checker import *
 from solver import *
 
 board_path = None
 goal_path = None
 
+try:
+	# board_path = "puzzles/hard/big.tray.4"
+	# goal_path = "puzzles/hard/many.blocks.20.goal"
+	board_path = sys.argv[1]
+	goal_path = sys.argv[2]
+except Exception:
+	pass
 
-board_path = sys.argv[1]
-goal_path = sys.argv[2]
+#if os.name == 'nt': os.system('cls')
 
 
-with open(board_path) as bf:
+def run_tests():
+
+	easy_puzzles = []
+	medium_puzzles = []
+	hard_puzzles = []
+
+	with open("puzzles/easy_puzzles.csv") as easy_file:
+		puzzle_reader = csv.reader(easy_file)
+		for row in puzzle_reader:
+			puzzle_file = "easy/" + row[0].lstrip("(").replace('"', '')
+			goal_file = "easy/" + row[1].lstrip(" ").replace('"', '')
+			possible = row[2] == "False)"
+
+			easy_puzzles.append((puzzle_file, goal_file, possible))
 	
-	board_data = bf.read()
+	with open("puzzles/medium_puzzles.csv") as easy_file:
+		puzzle_reader = csv.reader(easy_file)
+		for row in puzzle_reader:
+			puzzle_file = "medium/" + row[0].lstrip("(").replace('"', '')
+			goal_file = "medium/" + row[1].lstrip(" ").replace('"', '')
+			possible = row[2] == "False)"
 
-	with open(goal_path) as gf:
-		goal_data = gf.read()
+			medium_puzzles.append((puzzle_file, goal_file, possible))
+	
+	with open("puzzles/hard_puzzles.csv") as easy_file:
+		puzzle_reader = csv.reader(easy_file)
+		for row in puzzle_reader:
+			puzzle_file = "hard/" + row[0].lstrip("(").replace('"', '')
+			goal_file = "hard/" + row[1].lstrip(" ").replace('"', '')
+			possible = row[2] == "False)"
 
-	board = board_from_string(board_data, goal_data)
-	board_copy = copy.deepcopy(board)
-	solution = solve(board)
+			hard_puzzles.append((puzzle_file, goal_file, possible))
 
-	if(solution == []):
-		print("-1")
-	else:
-		for move in solution:
-			print(move)
+	successful = 0
+	failed = 0
+	easy_marks = 40
+	medium_marks = 0
+	hard_marks = 0
+
+
+	for puzzle in easy_puzzles + medium_puzzles + hard_puzzles:
+		board_path = f"./puzzles/{puzzle[0]}"
+		goal_path = f"./puzzles/{puzzle[1]}"
+		possible = puzzle[2]
+
+		print(puzzle[0], end=' ')
+
+		
+		with open(board_path) as bf:
+			board_data = bf.read()
+
+		with open(goal_path) as gf:
+			goal_data = gf.read()
+
+		board = board_from_string(board_data, goal_data)
+		board_copy = copy.deepcopy(board)
+
+		try:
+			start_time = time.time()
+			solution = solve(board, False)
+			exec_time = time.time() - start_time
+			if exec_time > 60:
+				print("FAIL - OVERTIME", end="")
+				failed += 1
+				if puzzle in easy_puzzles:
+					easy_marks = 0
+			elif solution != []:
+				solution_correct = try_solution(board_copy, solution, False)
+				if(solution_correct == possible):
+					print("PASS", end="")
+					successful += 1
+					if puzzle in medium_puzzles:
+						medium_marks += 0.5
+					elif puzzle in hard_puzzles:
+						hard_marks += 1.5
+				else:
+					print("INCORRECT SOLUTION - FAIL", end="")
+					failed += 1
+					if puzzle in easy_puzzles:
+						easy_marks = 0
+			else:
+				if possible:
+					print("FAIL", end="")
+					failed += 1
+					if puzzle in easy_puzzles:
+						easy_marks = 0
+				else:
+					print("PASS", end="")
+					successful += 1
+					if puzzle in medium_puzzles:
+						medium_marks += 0.5
+					elif puzzle in hard_puzzles:
+						hard_marks += 1.5
+		except RecursionError:
+			sys.stdout.close()
+			print("FAIL (recursion depth exceeded)", end="")
+			failed += 1
+		
+		print(' (impossible)' if not possible else '')
+
+		
+	print(f"{successful} tests of {successful + failed} passed [{(successful * 100) / (successful + failed)}]%")
+	print(f"You will get {easy_marks + max(medium_marks, 19) + max(hard_marks, 41)} marks")
+
+if board_path == goal_path == None:
+	#cProfile.run("run_tests()", sort="tottime")
+	if os.name == 'nt': os.system('cls')
+	run_tests()
+else:
+	with open(board_path) as bf:
+		#sys.stdout = open(f"./output.txt", "w+", encoding="utf-8")
+		
+		board_data = bf.read()
+
+		with open(goal_path) as gf:
+			goal_data = gf.read()
+
+		board = board_from_string(board_data, goal_data)
+		board_copy = copy.deepcopy(board)
+		solution = solve(board, False)
+
+		if(solution == []):
+			print("-1")
+		else:
+			for move in solution:
+				print(move)
+
+		#print(try_solution(board_copy, solution, True))
+
+sys.stdout.close()
--- solver.py
+++ solver.py
@@ -6,18 +6,25 @@ import math
 def sign(x):
     return x / abs(x)
 
-def solve(board):
+def solve(board, debug=False):
 
     Block.col_sums = {}
     Block.row_sums = {}
 
+    if debug:
+        print("Goal:")
+        board.visualise(True)
+        print("Board:")
+        board.visualise()
+        
     if board.is_solved():
         return [Move(board.blocks[0].position, vec2(0, 0))] # 'null move'
     global searched_board_states
     searched_board_states = set()
-    solution = search_board(board)
+    solution = search_board(board, debug)
     return solution
 
+def explore_board(board, start_time, reverse_move_order=False, debug=False):
     global searched_board_states
     global iter_count
     moves_and_scores = []
@@ -47,12 +54,16 @@ def solve(board):
         if debug: print(f"checking block at ({block.position.x}, {block.position.y})")
         moves = block.get_available_moves(board)
         for move in moves:
+                if debug: print(f"    Found move {move}", end="")
                 board.make_move(move, False)
                 board_hash = hash(board)
+                if debug: print(f" {board_hash}", end="")
                 if not board_hash in searched_board_states:
                     searched_board_states.add(hash(board))
+                    if debug: print(f" with score {board.rank}")
                     moves_and_scores.append((move, board.rank))
                 else:
+                    if debug: print(f" (duplicate board)")
                     pass
                 board.undo_move(move, False)
 
@@ -60,7 +71,7 @@ def solve(board):
     return list(map(lambda x: x[0], moves_and_scores))
     
 
-def search_board(board):
+def search_board(board, debug):
 
     start_time = time.time()
 
@@ -69,8 +80,7 @@ def search_board(board):
     move_stack = Stack() # stack containing tuples of moves and the depth they must be made at
     
     # initialise stack with available moves from initial board state
-
-    moves_and_depths =  map(lambda x: (x, 0), explore_board(board, start_time True))
+    moves_and_depths =  map(lambda x: (x, 0), explore_board(board, start_time, True, debug))
     move_stack.extend(moves_and_depths)
 
     depth = 0
@@ -78,6 +88,7 @@ def search_board(board):
 
 
         if board.is_solved():
+            if debug: print("Solved!")
             return board.moves_made
 
         if time.time() - start_time > 59:
@@ -89,15 +100,18 @@ def search_board(board):
 
         for _ in range(depth_difference): # go back up to the depth required to make this move
             depth -= 1
+            if debug: print(f"{depth}<---------")
             board.undo_last()
             
+        if debug: board.visualise()
+        if debug: print(f"trying move {move} at depth {depth}")
         board.make_move(move, True, True)
         depth += 1
 
-        moves_and_depths =  map(lambda x: (x, depth), explore_board(board, start_time, True))
-
+        moves_and_depths =  map(lambda x: (x, depth), explore_board(board, start_time, True, debug))
         move_stack.extend(moves_and_depths)
 
     if board.is_solved():
+        if debug: print("Solved!")
         return board.moves_made
     return []
